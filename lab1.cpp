7d96a468a05a4795655fa909c8e79e19062d049f
Menhiss <mdunne697@gmail.com>
2026-01-13 14:48:03 -0500

    completed lab 1

---
+++ lab1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() {
+    // 2
+    // a)
+    int thisVariable, q76354, number;
+
+    // b)
+    std::cout << "Enter an integer: "; std::cin >> number;
+
+    // c)
+    std::cout << "This" << std::endl << "is" << std::endl << "a" << std::endl << "C++" << std::endl << "program." << std::endl;
+
+    // 3
+    // a)
+    std::cout << "Enter three integers: ";
+
+    // b)
+    int x, y, z;
+    std::cin >> x >> y >> z;
+    
+    // c)
+    int product = x * y * z;
+    std::cout << "The product of " << x << ", " << y << ", " << z << " is " << product << "." << std::endl;
+}
