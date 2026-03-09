2581ad1042ab2720b6fc42e7f47107f554e77e5d
Menhiss <mdunne697@gmail.com>
2026-03-09 14:55:07 -0400

    added midterm
    missed a few multiple choice

---
+++ midterm.cpp
@@ -0,0 +1,86 @@
+// multiple choice
+
+// what is output
+// int x = 15;
+// int y = 10;
+// int&p = x;
+// p = y;
+// 10
+
+// what does delete do
+// deallocate dynamic memory
+
+// which is false about for loop
+//  always executes once
+
+// i missed a few
+
+// which is true about recursion
+// uses call stack
+
+// which statement about std::vector is true
+// can resize dynamically
+
+// which causes memory leak
+// int* p = new int;
+
+// what is output
+// int f(int n) {
+// if (n==0) return 1;
+// return n*f(n-1);}
+// cout << f(3);
+// 6
+
+// which statement about exception handling is false
+// none of the above statements are false
+
+// all of the following codes decalre a 2-by-2 array and set its elements to 0
+// except array<array<int, 2>, 2> b; b{0};
+
+// assume a variable is a declared within a for statement initialization
+// expression. The variable
+// scope is restricted to the for statement
+
+// what happens if modifying data in const member fucntion
+// a compilation error
+
+// which of the following is false about constructors
+// constructor must return a value
+
+// which is true about references
+// must be initialized
+
+// function overloading depends on
+// parameter lists
+
+// which of this is true about the inline keyword
+// decreased the executioon time of a function but increases the program size
+
+// what is the output
+// int arr[3] = {10, 20, 30};
+// int* p = arr;
+// cout << *(p + 1);
+// 20
+
+// what is the defualt acess specifier for a class
+// private
+
+// short answer
+
+// i missed the constructor destructor question
+
+// struct vs class for gps and bank
+// he said all answers correct
+
+// vehicle class question
+// inheritance and parent and child classes
+
+// 3 bugs code
+// logical should have been += not =+
+// memory error can't do BankAccount* acc; must be BankAccount* acc = new BankAccount();
+// access error is the balance variable is private and can't be accessed directly
+
+// 2 dimesnional array max value recursively
+// im not doing this again
+
+// uml diagram question
